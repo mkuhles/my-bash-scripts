9a818b0bf223f8b635447e2ac441ea289976719d
mkuhles <melanie.kuhles@gmail.com>
2019-12-17 10:49:54 +0100

    createVHost on ubuntu

--- createVHost.sh
+++ createVHost.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 # VirtualHost für Domain erstellen (falls noch nicht passiert)
 # source https://www.webhosterwissen.de/know-how/eigener-webserver/tutorial-apache-virtual-hosts-anlegen/
- 
+
+if [ -z $APACHE_LOG_DIR ]; then
+  APACHE_LOG_DIR='/var/www/log'
+fi
+
 # Schritt 1 = domain, directory und public directory abfragen
 if [ -z $1 ]; then
     echo Name the new domain.
-    read -p "Domain: " DOMAIN 
+    read -p "Domainame (without .tdl): " DOMAIN_NAME
 else
-    DOMIAN=$1
-    echo Domain: $DOMAIN
+    DOMAIN_NAME=$1
+    echo "Domainame (without .tdl): $DOMAIN_NAME"
 fi
 
-echo Should the projectfolder has the name $DOMAIN \[Y/n\]
+echo Should the projectfolder has the name $DOMAIN_NAME \[Y/n\]
 read selection
 
 if [ $selection != 'Y' ]; then
     read -p 'Project Directory: ' DIR
 else
-    DIR=$DOMAIN
+    DIR=$DOMAIN_NAME
     echo Projects Directory: $DIR
 fi
 
-DIR=/var/www/win/$DIR
+DIR=/var/www/html/$DIR
+DOMAIN="$DOMAIN_NAME.test"
 
 read -p "Please enter the public directory relativ to the project directory [$DIR]: " PUB_DIR
 if [ -z $PUB_DIR ]; then
@@ -39,22 +44,66 @@ else
     sudo mkdir $DIR
 fi
 
-# Schritt 3 - Apache Config-Datei für domain.de erstellen
+# Schritt 3 create self singed cert
+cd ~/ssl-certs
+mkcert $DOMAIN
+
+
+# Schritt 4 - Apache Config-Datei für domain.de erstellen
+echo "create config file: /etc/apache2/sites-available/$DOMAIN.conf"
 printf "
 <VirtualHost *:80>
     ServerAdmin admin@$DOMAIN
     ServerName $DOMAIN
     ServerAlias www.$DOMAIN
     DocumentRoot $PUB_DIR
-    ErrorLog \${APACHE_LOG_DIR}/error.log
-    CustomLog \${APACHE_LOG_DIR}/access.log combined
+    ErrorLog ${APACHE_LOG_DIR}/$DOMAIN_NAME-error.log
+    CustomLog ${APACHE_LOG_DIR}/$DOMAIN_NAME-access.log combined
 </VirtualHost>
 			 
 <Directory $PUB_DIR/>
     AllowOverride All
-</Directory>" | sudo tee /etc/apache2/sites-available/$DOMAIN.conf
-			 
-# Schritt 3 - VirtualHost Konfiguration für Domain aktivieren
+</Directory>
+" | sudo tee /etc/apache2/sites-available/$DOMAIN.conf
+
+echo "create config file: /etc/apache2/sites-available/$DOMAIN-ssl.conf"
+printf "<IfModule mod_ssl.c>
+    <VirtualHost _default_:443>
+        ServerAdmin admin@$DOMAIN
+        ServerName $DOMAIN
+        ServerAlias www.$DOMAIN
+        DocumentRoot $PUB_DIR
+
+        <Directory $PUB_DIR/>
+            AllowOverride all
+        </Directory>
+
+        ErrorLog ${APACHE_LOG_DIR}/$DOMAIN_NAME-error.log
+        CustomLog ${APACHE_LOG_DIR}/$DOMAIN_NAME-access.log combined
+
+        SSLEngine on
+
+        SSLCertificateFile  $HOME/ssl-certs/$DOMAIN.pem
+        SSLCertificateKeyFile $HOME/ssl-certs/$DOMAIN-key.pem
+
+        <FilesMatch \"\\.(cgi|shtml|phtml|php)$\">
+                SSLOptions +StdEnvVars
+        </FilesMatch>
+        <Directory /usr/lib/cgi-bin>
+                SSLOptions +StdEnvVars
+        </Directory>
+
+    </VirtualHost>
+</IfModule>
+" | sudo tee /etc/apache2/sites-available/$DOMAIN-ssl.conf
+
+# Schritt 5 - VirtualHost Konfiguration für Domain aktivieren
 sudo a2ensite $DOMAIN.conf
+sudo a2ensite $DOMAIN-ssl.conf
 sudo service apache2 reload
 
+# Schritt 6 - add new url to hosts file
+
+echo "127.0.0.1  $DOMAIN
+127.0.0.1  www.$DOMAIN
+" | sudo tee -a /etc/hosts > /dev/null
