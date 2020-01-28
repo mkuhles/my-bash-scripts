c974d446f60040ed2930aef8c13790f720c2f432
mkuhles <melanie.kuhles@quadriga.eu>
2020-01-28 11:53:05 +0100

    request db name, user and password

--- createDB.sh
+++ createDB.sh
@@ -2,14 +2,16 @@
 
 # source: https://stackoverflow.com/questions/33470753/create-mysql-database-and-user-in-bash-script 
 
-MAINDB=$1
-PASSWDDB=$1
+read -p "Database: " DB
+read -p "User: " USER
+echo "Password: "
+read -s PASSWD
 
 # If /root/.my.cnf exists then it won't ask for root password
 if [ -f /root/.my.cnf ]; then
-	mysql -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-	mysql -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
-	mysql -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+	mysql -e "CREATE DATABASE ${DB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+	mysql -e "CREATE USER ${USER}@localhost IDENTIFIED BY '${PASSWD}';"
+	mysql -e "GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';"
 	mysql -e "FLUSH PRIVILEGES;"
 
 	# If /root/.my.cnf doesn't exist then it'll ask for root password   
@@ -17,8 +19,8 @@ else
 	echo "Please enter root user MySQL password!"
 	echo "Note: password will be hidden when typing"
 	read -s rootpasswd
-	sudo mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
-	sudo mysql -uroot -p${rootpasswd} -e "CREATE USER ${MAINDB}@localhost IDENTIFIED BY '${PASSWDDB}';"
-	sudo mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${MAINDB}'@'localhost';"
+	sudo mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${DB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+	sudo mysql -uroot -p${rootpasswd} -e "CREATE USER ${USER}@localhost IDENTIFIED BY '${PASSWD}';"
+	sudo mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${DB}.* TO '${USER}'@'localhost';"
 	sudo mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
 fi
